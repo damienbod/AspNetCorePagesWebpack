38d956a7ba247595a51d8693d93590e90d5ee078
damienbod <damien_bod@hotmail.com>
2018-07-01 18:25:02 +0200

    adding a translation for the callback url

--- AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -93,8 +93,10 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account
                         values: new { userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, _identityLocalizer["CONFIRM_YOUR_EMAIL"],
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(
+                        Input.Email, 
+                        _identityLocalizer["CONFIRM_YOUR_EMAIL"],
+                        _identityLocalizer["CONFIRM_YOUR_EMAIL_TEXT", HtmlEncoder.Default.Encode(callbackUrl)]);
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);

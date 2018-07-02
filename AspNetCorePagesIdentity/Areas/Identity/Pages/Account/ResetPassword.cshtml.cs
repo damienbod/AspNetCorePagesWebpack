cafd89a7511d707058427e4f224eb2ae4b6504ff
damienbod <damien_bod@hotmail.com>
2018-07-02 07:23:37 +0200

    Reset Password

--- AspNetCorePagesIdentity/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ AspNetCorePagesIdentity/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -25,18 +25,17 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "EMAIL_REQUIRED")]
             [EmailAddress]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "PASSWORD_REQUIRED")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "CONFIRM_PASSWORD_NOT_MATCHING")]
             public string ConfirmPassword { get; set; }
 
             public string Code { get; set; }

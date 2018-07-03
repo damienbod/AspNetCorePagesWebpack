edafb6fb35a42425d5c763e49cba2e5c1784bf49
damienbod <damien_bod@hotmail.com>
2018-07-03 13:09:56 +0200

    manage translations

--- AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using AspNetCorePagesIdentity.Resources;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 
 namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
@@ -10,13 +13,19 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<PersonalDataModel> _logger;
+        private readonly IStringLocalizer _identityLocalizer;
 
         public PersonalDataModel(
             UserManager<IdentityUser> userManager,
-            ILogger<PersonalDataModel> logger)
+            ILogger<PersonalDataModel> logger,
+            IStringLocalizerFactory factory)
         {
             _userManager = userManager;
             _logger = logger;
+
+            var type = typeof(IdentityResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _identityLocalizer = factory.Create("IdentityResource", assemblyName.Name);
         }
 
         public async Task<IActionResult> OnGet()
@@ -24,7 +33,7 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound(_identityLocalizer["USER_NOTFOUND", _userManager.GetUserId(User)]);
             }
 
             return Page();

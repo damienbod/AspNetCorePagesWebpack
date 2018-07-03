a856b1eb6c0fc96347b60e258c3484a7e740ddce
damienbod <damien_bod@hotmail.com>
2018-07-03 09:18:36 +0200

    changed password translations

--- AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ AspNetCorePagesIdentity/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AspNetCorePagesIdentity.Resources;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
 {
@@ -14,15 +17,21 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<ChangePasswordModel> _logger;
+        private readonly IStringLocalizer _identityLocalizer;
 
         public ChangePasswordModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            ILogger<ChangePasswordModel> logger)
+            ILogger<ChangePasswordModel> logger,
+            IStringLocalizerFactory factory)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+
+            var type = typeof(IdentityResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _identityLocalizer = factory.Create("IdentityResource", assemblyName.Name);
         }
 
         [BindProperty]
@@ -33,20 +42,17 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required(ErrorMessage = "PASSWORD_REQUIRED")]
+            [Required(ErrorMessage = "CURRENT_PASSWORD_REQUIRED")]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            [Required(ErrorMessage = "PASSWORD_REQUIRED")]
+            [Required(ErrorMessage = "NEW_PASSWORD_REQUIRED")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Compare("NewPassword", ErrorMessage = "CONFIRM_PASSWORD_NOT_MATCHING")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -55,7 +61,7 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound(_identityLocalizer["CHANGE_PASSWORD_USER_NOTFOUND", _userManager.GetUserId(User)]);
             }
 
             var hasPassword = await _userManager.HasPasswordAsync(user);
@@ -77,7 +83,7 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound(_identityLocalizer["CHANGE_PASSWORD_USER_NOTFOUND", _userManager.GetUserId(User)]);
             }
 
             var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
@@ -92,7 +98,7 @@ namespace AspNetCorePagesIdentity.Areas.Identity.Pages.Account.Manage
 
             await _signInManager.RefreshSignInAsync(user);
             _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
+            StatusMessage = _identityLocalizer["CHANGE_PASSWORD_STATUS"];
 
             return RedirectToPage();
         }
